cc11b635f5dc09a390cf697518b024a24d52d5f4
Kemal Erdem <kemalpiro@gmail.com>
2024-01-09 10:51:36 +0100

    playground

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,7 +1,12 @@
+import {Link} from "@nextui-org/react";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
-      test
+
+      <Link  href={'/login'} isBlock showAnchorIcon color="secondary">
+        GO TO LOGIN
+      </Link>
     </main>
   )
 }
